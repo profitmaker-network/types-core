871f232c39cc8d1ac4567cb68d01843243b2f166
Eugen Soloviov <suenot@gmail.com>
2021-10-24 15:03:46 +0300

    add Id interface

--- types.d.ts
+++ types.d.ts
@@ -1,33 +1,36 @@
-export interface OrderBook {
-    id?: string,
-    pair: string,
-    isin?: string,
-    exchange: string,
-    broker: string,
-    timestamp: number,
-    trade_status?: boolean,
-    bids: Order[],
-    asks: Order[],
-    min_price_increment?: number
-  }
+export interface Id {
+  id: string
+}
+
+export interface OrderBook extends Id {
+  pair: string,
+  isin?: string,
+  exchange: string,
+  broker: string,
+  timestamp: number,
+  trade_status?: boolean,
+  bids: Order[],
+  asks: Order[],
+  min_price_increment?: number
+}
   
-  // it can be the user market position or orderbook order
-  export type Order = {
-    price: number,
-    amount: number,
-    total?: number,
-    percent?: number,
-    sum?: number,
-    wall?: boolean,
-    user?: string,
-    pair?: string,
-    isin?: string,
-    exchange?: string,
-    broker?: string,
-    timestamp?: number,
-    trade_status?: string,
-    side?: string,
-    fee?: number,
-    user_order?: boolean
-  }
+// it can be the user market position or orderbook order
+export interface Order extends Id {
+  price: number,
+  amount: number,
+  total?: number,
+  percent?: number,
+  sum?: number,
+  wall?: boolean,
+  user?: string,
+  pair?: string,
+  isin?: string,
+  exchange?: string,
+  broker?: string,
+  timestamp?: number,
+  trade_status?: string,
+  side?: string,
+  fee?: number,
+  user_order?: boolean
+}
   
